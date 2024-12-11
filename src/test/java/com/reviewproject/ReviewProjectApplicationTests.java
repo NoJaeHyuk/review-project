d51df0eddb1899d9d1ee6a6ebacc92398b784170
NOJaeHyuk <jaehyuk7722@naver.com>
2024-12-11 17:02:50 +0900

    first project commit

---
+++ src/test/java/com/reviewproject/ReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
